cf324e794e361981f72c6714192976eaf6d745d6
Sthealth <AW@example.com>
2025-09-30 15:54:06 -0400

    add attributes to Cellphone class and add prompt user in main

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("What is the serial number? ");
+        int userSerialNumber = myScanner.nextInt();
+        myScanner.nextLine();
+
+        System.out.print("What model is the phone? ");
+        String userPhoneModel = myScanner.nextLine();
+
+        System.out.print("Who is the carrier? ");
+        String userCarrier = myScanner.nextLine();
+
+        System.out.print("What is the phone number? ");
+        String userPhoneNumber = myScanner.nextLine();
+
+        System.out.print("Who is the owner of the phone? ");
+        String ownerName = myScanner.nextLine();
+
+        Cellphone phoneInformation = new Cellphone();
+
+        phoneInformation.setSerialNumber(userSerialNumber);
+        phoneInformation.setModel(userPhoneModel);
+        phoneInformation.setCarrier(userCarrier);
+        phoneInformation.setPhoneNumber(userPhoneNumber);
+        phoneInformation.setOwner(ownerName);
+
+        System.out.println();
+        System.out.println(phoneInformation.getSerialNumber());
+        System.out.println(phoneInformation.getModel());
+        System.out.println(phoneInformation.getCarrier());
+        System.out.println(phoneInformation.getPhoneNumber());
+        System.out.println(phoneInformation.getOwner());
+    }
 }
